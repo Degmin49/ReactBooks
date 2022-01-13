808e1f864493fe3f90fa48d7f1f990c28b6ff2b3
Daniel 114103 <pzx97264@student.wsb.poznan.pl>
2022-01-13 16:10:57 +0100

    dziala

--- reactbooks/src/components/Navbar.js
+++ reactbooks/src/components/Navbar.js
@@ -1,28 +1,14 @@
 import React from 'react';
 
-// const Navbar = () => {
-//     return(
-//         // <>
-//         //     <Nav className='navbar'>
-//         //         <div className='navbar-container'>
-//         //             <Link to="/" className="navbar-logo"></Link>
-//         //         </div>
-//         //     </Nav>
-//         // </>
-//         <div>Hello!</div>);
-// }
-
-// export default Navbar;
-
-// function Navbar() {
-//     return(<div>Hello!</div>);
-// }
-
-// export default Navbar;
-
 const Navbar = () => {
-    return (
-        <div>Hello from Navbar!</div>
+    return(
+        <>
+            {/* <Nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to="/" className="navbar-logo"></Link>
+                </div>
+            </Nav> */}
+        </>
     );
 }
 
