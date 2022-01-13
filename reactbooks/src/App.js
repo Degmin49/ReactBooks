99c81f721cec68971fad714b06aca2e2c23a9660
Daniel 114103 <pzx97264@student.wsb.poznan.pl>
2022-01-13 16:43:14 +0100

    Bootstrap

--- reactbooks/src/App.js
+++ reactbooks/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
-import Navbar from './components/Navbar';
+//import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <Navbar />
+    <div className='App'>
+      <Navbar bg='dark' variant='dark' sticky='top'>
+        <Navbar.Brand>
+          {/* <img src={logo} /> */}
+          Logo
+        </Navbar.Brand>
+      </Navbar>
+      <div className='content'>
+
+      </div>
+    </div>
   );
 }
 
