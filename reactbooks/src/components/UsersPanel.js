eca0531c6ec0e8f03eaa419a818b37c5690e6356
Daniel 114103 <pzx97264@student.wsb.poznan.pl>
2022-01-14 12:59:55 +0100

    zmiana na klase

--- reactbooks/src/components/UsersPanel.js
+++ reactbooks/src/components/UsersPanel.js
@@ -10,56 +10,92 @@ var Users = [
     {userid: 4,type: false,name: "Arek",surname: "Milik",email: "siła@nie-technika.pl"}
 ];
 // Users = [];
-var UsersList = Users.map((x,index) => 
-    <tr key={x.userid}>
-        <td>{index+1}</td>
-        <td>{x.name} {x.type ? <span title="Konto administratora">✔</span> : ''}</td>
-        <td>{x.surname}</td>
-        <td>{x.email}</td>
-        <td>{x.type ? '' : <button type="button" onClick={() => edit(x)} className="btn btn-outline-secondary">Edytuj</button>}</td>
-    </tr>);
 
-function edit(x) {
-    
-    console.log("Imie: "+x.name); //co tutaj się dzieje
-}
-
-const UsersPanel = () => {
-    let content;
-    if (Users.length > 0) {
-        content = 
-            <div>
-                <span className="MyTitle">Lista użytkowników:<br /><br /></span>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Lp.</th>
-                            <th>Imię</th>
-                            <th>Nazwisko</th>
-                            <th>E-mail</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {UsersList}
-                        <tr key={-1}>
-                            <th>Lp.</th>
-                            <th>Imię</th>
-                            <th>Nazwisko</th>
-                            <th>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            </th>
-                        </tr>
-                    </tbody>
-                </Table>
-            </div>
+class UsersPanel extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            users: Users
+        };
     }
-    else {content = <div className="MyTitleGreen">Nie ma żadnych użytkowników!</div>}
     
-    return(
-        <Container className="MyContainer">
-            {content}
-        </Container>
-    );
+    showContent(){
+        // if (Users.length > 0) {
+        //     this.setState({content: 
+        //         <div>
+        //             <span className="MyTitle">Lista użytkowników:<br /><br /></span>
+        //             <Table>
+        //                 <thead>
+        //                     <tr>
+        //                         <th>Lp.</th>
+        //                         <th>Imię</th>
+        //                         <th>Nazwisko</th>
+        //                         <th>E-mail</th>
+        //                     </tr>
+        //                 </thead>
+        //                 <tbody>
+        //                     {UsersList}
+        //                     <tr key={-1}>
+        //                         <th>Lp.</th>
+        //                         <th>Imię</th>
+        //                         <th>Nazwisko</th>
+        //                         <th>
+        //                         <Form.Control type="email" placeholder="Enter email" />
+        //                         </th>
+        //                     </tr>
+        //                 </tbody>
+        //             </Table>
+        //         </div>
+        //     });
+        // }
+        // else {
+        //     this.setState({content: 
+        //         <div className="MyTitleGreen">Nie ma żadnych użytkowników!</div>
+        //     });
+        // }
+    }
+
+    render(){
+        const UsersList = this.state.users.map((x,index) => {
+        return (
+            <tr key={x.userid}>
+                <td>{index+1}</td>
+                <td>{x.name} {x.type ? <span title="Konto administratora">✔</span> : ''}</td>
+                <td>{x.surname}</td>
+                <td>{x.email}</td>
+                <td>{x.type ? '' : <button type="button" className="btn btn-outline-secondary">Edytuj</button>}</td>
+            </tr>
+        )});
+        
+        return(
+            <Container className="MyContainer">
+                <div>
+                    <span className="MyTitle">Lista użytkowników:<br /><br /></span>
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>Lp.</th>
+                                <th>Imię</th>
+                                <th>Nazwisko</th>
+                                <th>E-mail</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {UsersList}
+                            <tr key={-1}>
+                                <th>Lp.</th>
+                                <th>Imię</th>
+                                <th>Nazwisko</th>
+                                <th>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                </th>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+            </Container>
+        );
+    }
 }
 
 export default UsersPanel
