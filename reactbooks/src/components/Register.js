fd5359945b8ecfd2cad42fc686e5c97ae19dd98a
Daniel 114103 <pzx97264@student.wsb.poznan.pl>
2022-01-15 17:04:35 +0100

    ok

--- reactbooks/src/components/Register.js
+++ reactbooks/src/components/Register.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 
 class Register extends React.Component {
     constructor(props){
@@ -21,39 +21,59 @@ class Register extends React.Component {
         console.log("rejestrujemy użytkownika: "+registerUser.name+" "+registerUser.surname+" "+registerUser.email+" "+registerUser.password); //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
         console.log("Jeśli serwer odpowie to wyświetlamy Modal");
         //this.setState({ showModalSuccess: true});
-        //this.setState({ showModalFail: true});
+        this.setState({ showModalFail: true});
     }
 
     render(){
         return(
-            <Form className="MyLogin">
-                <div className="MyLoginInfo">Zarejestruj się</div>
+            <div>
+                <Form className="MyLogin">
+                    <div className="MyLoginInfo">Zarejestruj się</div>
 
-                <div>
-                    <label>Podaj imię:</label>
-                    <input type="name" id="registerName" className="form-control" placeholder="Wpisz swoje imię" />
-                </div>
+                    <div>
+                        <label>Podaj imię:</label>
+                        <input type="name" id="registerName" className="form-control" placeholder="Wpisz swoje imię" />
+                    </div>
 
-                <div>
-                    <label>Podaj nazwisko:</label>
-                    <input type="surname" id="registerSurname" className="form-control" placeholder="Wpisz swoje nazwisko" />
-                </div>
+                    <div>
+                        <label>Podaj nazwisko:</label>
+                        <input type="surname" id="registerSurname" className="form-control" placeholder="Wpisz swoje nazwisko" />
+                    </div>
 
-                <div>
-                    <label>Podaj e-mail:</label>
-                    <input type="email" id="registerEmail" className="form-control" placeholder="Wpisz swój adres e-mail" />
-                </div>
+                    <div>
+                        <label>Podaj e-mail:</label>
+                        <input type="email" id="registerEmail" className="form-control" placeholder="Wpisz swój adres e-mail" />
+                    </div>
 
-                <div>
-                    <label>Podaj hasło:</label>
-                    <input type="password" id="registerPassword" className="form-control" placeholder="Wpisz swoje hasło" />
-                </div>
+                    <div>
+                        <label>Podaj hasło:</label>
+                        <input type="password" id="registerPassword" className="form-control" placeholder="Wpisz swoje hasło" />
+                    </div>
 
-                <div className="MyCenterRegister">
-                    <Button className="btn btn-dark btn-lg btn-block" onClick={() => {this.register()}}>Zarejestruj</Button>
-                </div>
-                <div className="MyRegisterLink">Masz już konto? <a href="./Login">Zaloguj się!</a></div>
-            </Form>
+                    <div className="MyCenterRegister">
+                        <Button className="btn btn-dark btn-lg btn-block" onClick={() => {this.register()}}>Zarejestruj</Button>
+                    </div>
+                    <div className="MyRegisterLink">Masz już konto? <a href="./Login">Zaloguj się!</a></div>
+                </Form>
+
+                <Modal show={this.state.showModalSuccess} onHide={() => {this.setState({showModalSuccess: false})}}>
+                    <Modal.Header closeButton><Modal.Title>Konto zostało utworzone!</Modal.Title></Modal.Header>
+                    <Modal.Body>Twoje konto zostało pomyślnie zarejestrowanie! Możesz teraz przejść do strony logowania, żeby zalogować się do naszego serwisu!</Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="success" href="./Login" onClick={() => {this.setState({showModalSuccess: false})}}>Przejdź do logowania</Button>
+                    </Modal.Footer>
+                </Modal>
+
+                <Modal show={this.state.showModalFail} onHide={() => {this.setState({showModalFail: false})}}>
+                    <Modal.Header closeButton><Modal.Title>Nie można utworzyć konta!</Modal.Title></Modal.Header>
+                    <Modal.Body>Niestety, ale nie można utworzyć konta z powodu: </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="danger" onClick={() => {this.setState({showModalFail: false})}}>Spróbuj ponownie</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
         );
     }
 }
